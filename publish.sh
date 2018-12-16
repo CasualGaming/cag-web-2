826df9fc7556a779e15fff371a16b1f9db1ef915
Håvard O. Nordstrand <haavard.nordstrand@gmail.com>
2018-12-16 05:29:38 +0100

    Update theme and modify build script

--- publish.sh
+++ publish.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
 set -e
-shopt -s dotglob
 
 TIMESTAMP=$(date +"%Y-%m-%dT%H:%M:%S")
 
 # Make sure submodules are updated
+echo "Updating submodules ..."
 ./update-submodules.sh
 
-# Prepare master dir
+# Empty publish branch dir
+echo "Emptying publishing branch directory ..."
 cd public
-git fetch
-git checkout --force master
 find . -regextype sed -not -regex "^\\.$" -not -regex "^\\./\\.git$" -delete
 cd ..
 
 # Build
+echo "Building site ..."
 hugo -v
+echo "Build successful! (But do check for errors above manually.)"
 
 # Publish to master
-read -p "Press ENTER to publish, or CTRL+C to cancel"
+echo
+echo "Do you wish to publish the built site?"
+read -p "Press ENTER to continue, or CTRL+C to cancel."
 cd public
 git add -A
 git commit --allow-empty -m "Build $TIMESTAMP"
 git push --force
+echo "Publish successful!"
--- update-submodules.sh
+++ update-submodules.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
+THEME="fifty"
+
 git fetch
 git submodule update --init --recursive --force
+
+cd public
+git fetch
+git checkout --force master
+cd ..
+
+cd "themes/$THEME"
+git fetch
+git checkout --force master
+cd ../..
