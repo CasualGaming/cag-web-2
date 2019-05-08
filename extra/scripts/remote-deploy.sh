4b78f8e674f38d5de5eda7c9ec41834423f5df65
Håvard O. Nordstrand <haavard@haavard.tech>
2019-05-08 12:44:44 +0200

    Add description to extra script

--- extra/scripts/remote-deploy.sh
+++ extra/scripts/remote-deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Deployment script for CaG Web II for CI.
+# PWD does not need to be the app dir when calling this script.
+#
+# To call it from a deployment user, add a line like this to the sudoers file:
+# deploy ALL=(root) NOPASSWD: /srv/cag-web-2/remote-deploy.sh
+
 APP_DIR="/srv/cag-web-2"
 LOG_FILE="remote-deploy.log"
 
