ab0b7deea6618df388074e1c4e572f1bda9a79d2
Håvard O. Nordstrand <haavard@haavard.tech>
2020-08-26 15:15:11 +0200

    Update banner image

--- manage/docker-run.sh
+++ manage/docker-run.sh
@@ -3,7 +3,13 @@
 set -eu
 
 IMAGE_NAME="cag-web-2:dev"
-ENDPOINT="[::1]:1313"
+ENDPOINT="127.0.0.1:1313"
 
 echo "Running server ..."
-docker run --rm -p"$ENDPOINT:80/tcp" "$IMAGE_NAME"
+cmd="docker run --rm -it -p"$ENDPOINT:80/tcp" "$IMAGE_NAME""
+
+if [[ $(uname -s) == "MINGW"* ]]; then
+    winpty $cmd
+else
+    $cmd
+fi
